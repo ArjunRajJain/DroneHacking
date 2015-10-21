abdfefa9d584667772cd78a0d2eca609c043f3f2
bhumikab27 <bhumikabhavsar27@gmail.com>
2015-10-21 12:52:01 -0400

    Made the changes for comments

--- Final/util.c
+++ Final/util.c
@@ -1,86 +1,72 @@
 #include "demo.h"
 
 /*
- * lookup -
- * returns the "value" part of a <name, value>
- * pair when given "name"; NULL if not found.
+ * lookup function -->
+ * returns the "value" part of a <name, value> pair when given "name"; NULL if not found.
+ * Here it returns the password for the given username
  */
+char *lookup( name )
+  char *name;
+  {
+    struct sym_list *sp;
 
-char *
-lookup( name )
-char *name;
-{
-  struct sym_list *sp;
-
-  sp = s_lookup( name );
-  if( sp != (struct sym_list *) NULL )
-    return( sp->s_sym.v_value );
-  else
-    return( (char *) NULL );
-}
+    sp = s_lookup( name );
+    if( sp != (struct sym_list *) NULL )
+      return( sp->s_sym.v_value );
+    else
+      return( (char *) NULL );
+  }
 
 /*
- * insert()
- * inserts a new <name,value> pair into the associative memory,
- * which is maintained as a linked list pointed to by "Head".
+ * insert() function -->
+ * inserts a new <name,value> pair into the database file which is then stored
  */
-
 insert( name, value )
-char *name, *value;
-{
-  struct sym_list *sp;
+  char *name, *value;
+  {
+    struct sym_list *sp;
+    sp = s_lookup( name );
 
-  sp = s_lookup( name );
+    /* if the name exists in the database file, nothing is changed.
+     * Otherwise, a new element is inserted at the beginning of the list in the file. 
+    */
 
-  /* if it's already in the list, we won't
-   * change the list structure. Otherwise,
-   * we'll insert a new element at the beginning
-   * of the list. 
-   */
+    if( sp == (struct sym_list * ) NULL )
+      {
+        sp = new_sym();
+        if( sp == (struct sym_list *) NULL )
+  	     {
+      	  printf( "Fatal error: no memory\n" );
+      	  exit( 1 );
+  	     }
+  	    sp->s_next = Head.s_next;
+        Head.s_next = sp;
+      }
 
-  if( sp == (struct sym_list * ) NULL )
-    {
-      sp = new_sym();
-      if( sp == (struct sym_list *) NULL )
-	{
-	  printf( "Fatal error: no memory\n" );
-	  exit( 1 );
+    /* one way or another, "sp" now points to a valid entry. */
+    sp->s_sym.v_name = strsave( name );
+    sp->s_sym.v_value = strsave( value );
 	}
-	
-      sp->s_next = Head.s_next;
-      Head.s_next = sp;
-    }
 
-  /* one way or another, "sp" now points to a valid entry. */
-
-  sp->s_sym.v_name = strsave( name );
-  sp->s_sym.v_value = strsave( value );
-	
-}
-
-/*
+/* strsave()
  * returns a pointer to a copy of the 
  * character string pointed to by "str".
  */
+char *strsave( str )
+  char *str;
+  {
+  	char *p;
 
-char *
-strsave( str )
-char *str;
-{
-	char *p;
+  	p = (char *) malloc( strlen(str)+1 );
+  	if( p != (char *) NULL )
+  		strcpy( p, str );
+  	return( p );
+  }
 
-	p = (char *) malloc( strlen(str)+1 );
-	if( p != (char *) NULL )
-		strcpy( p, str );
-	return( p );
-}
-
-/*
+/*new_sym()
  * provides a pointer to a new sym_list structure
  */
- 
-struct sym_list *
-new_sym()
+struct sym_list *new_sym()
 {
   struct sym_list *p;
 
@@ -93,92 +79,81 @@ new_sym()
 	
 }
 
-/*
- * s_lookup:
- * finds a symlist structure on the list pointed to by
- * Head which has a v_name entry matching name.
+/* s_lookup:
+ * finds a symlist structure on the list pointed to by Head which has a v_name entry matching name.
  */
 
-struct sym_list *
-s_lookup( name )
-char *name;
-{
-  struct sym_list *sp;
+struct sym_list *s_lookup( name )
+  char *name;
+  {
+    struct sym_list *sp;
 
 
-  for( sp = Head.s_next; sp != (struct sym_list *) NULL; sp = sp->s_next)
-    {
-      if( strcmp( sp->s_sym.v_name, name ) == 0 )
-	return( sp );
-    }
+    for( sp = Head.s_next; sp != (struct sym_list *) NULL; sp = sp->s_next)
+      {
+        if( strcmp( sp->s_sym.v_name, name ) == 0 )
+  	return( sp );
+      }
 
-  return( (struct sym_list *) NULL );
-}
+    return( (struct sym_list *) NULL );
+  }
 
 /* save() -
- *  saves the set of main memory <name, value> pairs
- *  to the disk file named in the character string "file".
- *
+ *  - saves the set of <name, value> pairs in the database file
  */
 
 save( file )
-char *file;
-{
-  FILE *filep;
-  struct sym_list *sp;
+  char *file;
+  {
+    FILE *filep;
+    struct sym_list *sp;
 
-  filep = fopen( file, "w" );
+    filep = fopen( file, "w" );
 
-  if( filep == (FILE *) NULL )
-    {
-      fputs( "Fatal error. Cannot Save", stderr );
-      exit( 1 );
-    }
+    if( filep == (FILE *) NULL )
+      {
+        fputs( "Fatal error. Cannot Save", stderr );
+        exit( 1 );
+      }
 
-  for( sp = Head.s_next;
-       sp != (struct sym_list *) NULL;
-       sp = sp->s_next )
-    {
-      fprintf( filep, "%s%c%s",
-	       sp->s_sym.v_name,
-	       EQUALS,
-	       sp->s_sym.v_value );
-    }
+    for( sp = Head.s_next;
+         sp != (struct sym_list *) NULL;
+         sp = sp->s_next )
+      {
+        fprintf( filep, "%s%c%s",
+  	       sp->s_sym.v_name,
+  	       EQUALS,
+  	       sp->s_sym.v_value );
+      }
 
-  fclose( filep );
-}
+    fclose( filep );
+  }
 
-/*
- *
- * restore()
- *  - restores the associative memory with the contents
- *    of the data file named by "file". It expects
- *    to find the input file organized in the same
- *    fashion as the input to the associative memory system.
+/* restore()
+ *  - restores the database file with the contents entered by the user.
  */
 
-void
-restore( file )
-char *file;
-{
-  FILE *db;
-  char buf[BUFSIZE];
+void restore( file )
+  char *file;
+  {
+    FILE *db;
+    char buf[BUFSIZE];
 
-  db = fopen( file, "r" );
+    db = fopen( file, "r" );
 
-  if( db != (FILE *) NULL )
-    {
-      while( fgets( buf, BUFSIZE, db ) > 0 )
-	{
-	  char *ptr;
-	      
-	  if( (ptr = find_equals( buf ) ) > 0 )
-	    {
-	      *ptr = EOS;
-	      insert( strsave( buf), strsave( ++ptr ));
-	    }
-	}
-      fclose( db );
-    }
-  return;
+    if( db != (FILE *) NULL )
+      {
+        while( fgets( buf, BUFSIZE, db ) > 0 )
+  	{
+  	  char *ptr;
+  	      
+  	  if( (ptr = find_equals( buf ) ) > 0 )
+  	    {
+  	      *ptr = EOS;
+  	      insert( strsave( buf), strsave( ++ptr ));
+  	    }
+  	}
+        fclose( db );
+      }
+    return;
 }
